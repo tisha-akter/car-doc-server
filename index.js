78b6b1c95fad1c47a4c2815f082eb34fa6fa2b89
tisha-akter <tishaakter.jui79@gmail.com>
2023-05-16 17:19:18 +0600

    verified and decoded JWT token

--- index.js
+++ index.js
@@ -24,6 +24,28 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+const verifyJWT = (req, res, next) => { 
+  console.log('hitting verify JWT')
+  console.log(req.headers.authorization);
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error: true, message: 'unauthorized access'})
+  }
+
+  const token = authorization.split(' ')[1];
+  console.log('token inside verify JWT', token);
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if(error){
+      return res.status(403).send({error: true, message: 'unauthorized access'})
+    }
+    req.decoded = decoded;
+    next();
+  })
+
+}
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -65,8 +87,10 @@ async function run() {
     })
 
     // checkOuts or bookings  routes
-    app.get('/checkOuts', async(req, res) => {
-      console.log(req. query.email);
+    app.get('/checkOuts', verifyJWT, async(req, res) => {
+
+      console.log('came back after verify');
+
       let query = {};
       if(req.query?.email){
         query = {email: req.query.email}
